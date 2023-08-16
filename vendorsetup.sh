5e899232159debb73912081aac16989e6206bb4a
hobby-lc <456042+hobby-lc@users.noreply.github.com>
2023-08-16 15:51:17 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_virgo-perf1-user
-add_lunch_combo omni_virgo-perf1-userdebug
-add_lunch_combo omni_virgo-perf1-eng
+add_lunch_combo omni_virgo_perf1-user
+add_lunch_combo omni_virgo_perf1-userdebug
+add_lunch_combo omni_virgo_perf1-eng
